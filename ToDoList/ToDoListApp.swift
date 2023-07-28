7bd3956914c00568b4f18671a28b74bf8f506d45
NZhukov <nzhukov.rnd@gmail.com>
2023-07-28 15:15:16 +0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Nikolay Zhukov on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
