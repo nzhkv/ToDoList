a9794c6df2eea65dd15f861ebc1b8437af197da8
NZhukov <nzhukov.rnd@gmail.com>
2023-07-30 15:07:14 +0400

    create all view

---
+++ ToDoList/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  ToDoList
+//
+//  Created by Nikolay Zhukov on 30.07.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ ToDoList/Views/ToDoListItemsView.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListItemsView.swift
+//  ToDoList
+//
+//  Created by Nikolay Zhukov on 30.07.2023.
+//
+
+import SwiftUI
+
+struct ToDoListItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToDoListItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListItemsView()
+    }
+}
