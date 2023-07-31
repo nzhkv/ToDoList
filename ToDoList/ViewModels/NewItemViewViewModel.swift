0ff90e8fd2df299eb3b8725ce48392692614ace0
NZhukov <nzhukov.rnd@gmail.com>
2023-07-31 16:01:57 +0400

    save

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Nikolay Zhukov on 30.07.2023.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -15,7 +17,28 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else { return }
         
+        // get current user ID
+        guard let uId = Auth.auth().currentUser?.uid else { return }
+        
+        // create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createDAte: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        // save
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictonary())
     }
     
     var canSave: Bool {
